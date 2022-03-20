4f6cf320d7381137ff742c34598118d964e36a29
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-03-19 18:14:56 -0600

    [ FlightSurety ]

---
+++ FlightSurety/src/dapp/index.js
@@ -0,0 +1,202 @@
+
+import DOM from './dom';
+import Contract from './contract';
+import './flightsurety.css';
+
+(async () => {
+    let result = null;
+    let contract = new Contract('localhost', () => {
+        // Read transaction
+        contract.isOperational((error, result) => {
+            console.log(error, result);
+            display('Operational Status: ', 'Check if contract is operational', [{label : 'Operational Status:', error : error, value : result}]);
+        });
+
+        // User-submitted transaction
+        DOM.elid('submit-oracle').addEventListener('click', () => {
+            let airline = DOM.elid('status-airline').value;
+            let flight = DOM.elid('status-flight').value;
+            
+            // Write transaction
+            contract.fetchFlightStatus(airline, flight, (error, result) => {
+                // display('Oracles', 'Trigger oracles', [{ label: 'Fetch Flight Status', error: error, value: JSON.stringify(result) }]);
+                console.log('Fetch Flight Status: ' + JSON.stringify(error));
+                displaySimple([{label : 'Fetch Flight Status', error : getErrorMessage(error), value : result}], "status-display-wrapper");
+            });
+        })
+
+        DOM.elid('register-airline').addEventListener('click', () => {
+            let airlineAddress = DOM.elid('airline-address').value;
+
+            contract.registerAirline(airlineAddress, (error, result) => {
+                //console.log('Registering airline ' + error.message);
+                displaySimple([{label : 'Registering airline', error : getErrorMessage(error), value: result }], "registration-display-wrapper");
+            });
+        })
+
+        DOM.elid('fund').addEventListener('click', () => {
+            let airlineAddress = DOM.elid('airline-address').value;
+
+            contract.fundAirline((error, result) => {
+                //displaySimple('Airline', 'fund', [{ label: 'fund airline', error: error, value: result.airline + ' ' + result.timestamp }]);
+                displaySimple([{label : 'Airline funding result', error : getErrorMessage(error), value : JSON.stringify(result)}], "funding-display-wrapper");
+            });
+        })
+
+        DOM.elid('buy-insurance').addEventListener('click', () => {
+            let airlineAddress = DOM.elid('insurance-airline').value;
+            let flight = DOM.elid('insurance-flight').value;
+            let amount = DOM.elid('insurance-amount').value;
+
+            contract.buyInsurance(flight, airlineAddress, amount, (error, result) => {
+                displaySimple([{label: 'Buy Insurance', error: getErrorMessage(error), value : JSON.stringify(result)}], "insurance-display-wrapper");
+            });
+        })
+
+        DOM.elid('claim-insurance').addEventListener('click', () => {
+            let airlineAddress = DOM.elid('insurance-airline').value;
+            let flight = DOM.elid('insurance-flight').value;
+
+            contract.creditPassenger(flight, airlineAddress, (error, result) => {
+                console.log('Pay passenger insurance money : ' + JSON.stringify(error) + ', result : ' + JSON.stringify(result));
+                displaySimple([{label : 'Pay passenger insurance money', error : getErrorMessage(error), value : JSON.stringify(result)}], "insurance-display-wrapper");
+            });
+        })
+
+        DOM.elid('check-claim').addEventListener('click', async () => {
+            let airlineAddress = DOM.elid('insurance-airline').value;
+            let flight = DOM.elid('insurance-flight').value;
+
+            let amount = await contract.checkCreditedMoney(flight, airlineAddress);
+            displaySimple([{label : 'checkCreditedMoney', error : "", value : JSON.stringify(amount) }], "insurance-display-wrapper");
+        })
+
+        DOM.elid('contract-balance').addEventListener('click', async () => {
+            let amount = await contract.getContractBalance();
+            displaySimple([{label : 'Contract balance', error: "", value: JSON.stringify(amount) }], "contract-display-wrapper");
+        })
+
+        DOM.elid('contract-authorize').addEventListener('click', async () => {
+            let result = await contract.authorizeCaller((error, result) => {
+                displaySimple([{ label: 'Authorize contract', error: getErrorMessage(error), value: JSON.stringify(result)}], "contract-display-wrapper");
+            });    
+        })
+    });
+
+    contract.flightSuretyApp.events.AirlineRegistered({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("Airline registered event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'Airline registered event', error: getErrorMessage(error), value : JSON.stringify(event.returnValues)}], "registration-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.AirlineRegistrationConsensus({
+        fromBlock: 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("Airline registration consensus event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'Airline registration consensus event', error: getErrorMessage(error), value: JSON.stringify(event.returnValues)}], "registration-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.AirlineFunded({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("Airline funded event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'Airline funded event', error: getErrorMessage(error), value: JSON.stringify(event.returnValues)}], "funding-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.InsurancePurchased({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("Insurance purchased event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'Insurance purchased event', error: getErrorMessage(error), value: JSON.stringify(event.returnValues)}], "insurance-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.PassengerCredited({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("Passenger credited event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'Passenger credited event', error : getErrorMessage(error), value : JSON.stringify(event.returnValues)}], "insurance-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.CreditInsurees({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("Credit insurees event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'Credit insurees event', error: getErrorMessage(error), value: JSON.stringify(event.returnValues)}], "credit-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.OracleRequest({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("OracleRequest event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'OracleRequest event', error: getErrorMessage(error), value: JSON.stringify(event.returnValues)}], "oracle-request-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.OracleReport({
+        fromBlock : 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("OracleReport event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'OracleReport event', error: getErrorMessage(error), value: JSON.stringify(event.returnValues)}], "oracle-report-display-wrapper");
+    });
+
+    contract.flightSuretyApp.events.FlightStatusInfo({
+        fromBlock: 0
+    }, function (error, event) {
+        if (error) console.log(error)
+        console.log("FlightStatusInfo event " + JSON.stringify(event.returnValues))
+        displaySimple([{ label: 'FlightStatusInfo event', error : getErrorMessage(error), value : JSON.stringify(event.returnValues)}], "status-display-wrapper");
+    });
+})();
+
+function display(title, description, results) {
+    let displayDiv = DOM.elid("display-wrapper");
+    let section = DOM.section();
+    section.appendChild(DOM.h2(title));
+    section.appendChild(DOM.h5(description));
+    
+    results.map((result) => {
+        let row = section.appendChild(DOM.div({className : 'row'}));
+        row.appendChild(DOM.div({ className: 'col-sm-4 field' }, result.label));
+        row.appendChild(DOM.div({ className: 'col-sm-8 field-value' }, result.error ? String(result.error) : String(result.value)));
+        section.appendChild(row);
+    })
+
+    displayDiv.append(section);
+}
+
+function displaySimple(results, divId) {
+    let displayDiv = DOM.elid(divId);
+    let section = DOM.section();
+    
+    results.map((result) => {
+        let row = section.appendChild(DOM.div({ className: 'row' }));
+        row.appendChild(DOM.p({ className: 'col-sm-12 field-value' }, result.label + ': ' +
+            (result.error ? String(result.error) : String(result.value))));
+        section.appendChild(row);
+    })
+
+    while (displayDiv.hasChildNodes()) {
+        displayDiv.removeChild(displayDiv.firstChild);
+    }
+
+    displayDiv.appendChild(section);
+}
+
+function getErrorMessage(error) {
+    let error2 = error;
+    try {
+        error2 = error.message;
+    } catch (e) {
+        console.log("error---> " + JSON.stringify(error));
+    }
+    
+    return error2;
+}
